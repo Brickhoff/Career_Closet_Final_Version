c4dc324e1207c16681ccc1c2b5ce2ddf5b55cb76
EC2 Default User <ec2-user@ip-172-31-39-58.us-east-2.compute.internal>
2018-04-06 19:41:13 +0000

    v3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,108 +1,31 @@
 class UsersController < ApplicationController
+  include UsersHelper
 
   def new
-    @user = User.new
+    @users = User.new
   end
   
   def index
-       redirect = false
-    
-    if params[:commit]
-      @commit = params[:commit]
-    else 
-      @commit = nil
-    end
-    
-    if @commit == "Clear" 
-      # flash.delete
-      session[:sort_by] = nil
-      session[:type] = nil
-      session[:search] = nil
-      redirect_to users_path :sort_by=>nil,:type => nil,:search => nil
-      return
-    end
-    
-    # if params[:search]
-    #   @user = User.search(params[:type],params[:search])
-    # else 
-    #   @user = User.all
-    # end
-    
-    
-    # if params[:sort_by]
-    #   @sort_by = params[:sort_by]
-    # end
-    # if @sort_by
-    #   @user = @user.order(@sort_by).all
-    # end
-    
-    if params[:type] and params[:search]
-      @type = params[:type] 
-      session[:type] = params[:type]
-      @search = params[:search] 
-      session[:search] = params[:search]
-    elsif session[:type] and session[:search]
-      @type = session[:type] 
-      @search = session[:search]
-      redirect = true
-    else
-      @type = nil
-      @search = nil
-    end
-      
-    if params[:sort_by]
-      @sort_by = params[:sort_by]
-      session[:sort_by] = params[:sort_by]
-    elsif session[:sort_by]
-      @sort_by = session[:sort_by]
-      redirect = true
-    else
-      @sort_by = nil
-    end  
-    
-    if redirect 
-      flash.keep
-      redirect_to users_path :sort_by=>@sort_by,:type => @type,:search => @search
-    end
-    
-    
-    if @search and @type and @sort_by
-      @user = User.search(@type,@search).order(@sort_by).all
-    elsif @search and @type
-      @user = User.search(@type,@search)
-    elsif @sort_by
-      @user = User.order(@sort_by).all
-    else 
-      @user = User.all
-    end
-  end
-  
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      user_log_in @user
-      flash[:success] = "Welcome to Career Closet #{@user.full_name}"
-      redirect_to appointments_path
-    else
-      render 'new'
-    end
+    @q_user = User.ransack(params[:q])
+    @users = @q_user.result().paginate(page: params[:page], :per_page => 30) || []
   end
   
   def edit
-      @user = User.find(params[:id])
+      @users = User.find(params[:id])
   end
   def update
-      @user = User.find(params[:id])
-      if @user.update_attributes(user_params)
+      @users = User.find(params[:id])
+      if @users.update_attributes(user_params)
           flash[:success] = "Your account was updated successfully"
-          redirect_to appointments_path
+          redirect_to appointments_path if user_logged_in?
+          redirect_to users_path if admin_logged_in? 
       else
           render 'edit'
       end
   end
   
   def show
-      @user = User.find(params[:id])
+      @users = User.find(params[:id])
   end
   
   private
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,37 @@
 module UsersHelper
+    def user_fields
+        [:first_name, :last_name, :uin, :phone, :email]
+    end
+    
+    def action
+        action_name == 'advance_search' ? :post : :get
+    end
+    
+    def display_user_sorted_column_headers(search)
+           string = content_tag(:th, sort_link(search, :first_name , "First Name", method: action))
+           string << content_tag(:th, sort_link(search, :last_name , "Last Name", method: action))
+           string << content_tag(:th, sort_link(search, :uin , "UIN", method: action))
+           string << content_tag(:th, sort_link(search, :phone , "Phone", method: action))
+           string << content_tag(:th, sort_link(search, :email , "E-mail", method: action))
+    end
+    
+    def display_user_sorted_search_results(objects)
+        objects.each_with_object('') do |object, string|
+            string << content_tag(:tr, display_user_sorted_search_results_row(object))
+        end
+    end
+    
+    def display_user_sorted_search_results_row(object)
+        fieldCount = 0
+        user_fields.each_with_object('') do |field, string|
+            if fieldCount == 0
+                string << content_tag(:td, link_to(object.send(field), '/users/' + object.id.to_s + '/edit'))
+            else
+                string << content_tag(:td, object.send(field))
+            end
+            
+            fieldCount += 1
+        end
+        .html_safe
+    end
 end
