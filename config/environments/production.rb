004ea9bd471b67bd7a2b46e06ecb7cdb9995e600
EC2 Default User <ec2-user@ip-172-31-39-58.us-east-2.compute.internal>
2018-04-17 21:43:57 +0000

    -v2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,18 +30,19 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
   
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  host = 'still-tor-19373.herokuapp.com/'
-  config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.gmail.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['tamuCodeZeta@gmail.com'],
-    :password       => ENV['CodeZetaPassword'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
+  config.action_mailer.default_url_options = { :host => 'still-tor-19373.herokuapp.com' }  
+  config.action_mailer.delivery_method = :smtp  
+  config.action_mailer.perform_deliveries = true  
+  config.action_mailer.raise_delivery_errors = false  
+  config.action_mailer.default :charset => "utf-8"  
+  config.action_mailer.smtp_settings = {  
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "still-tor-19373.herokuapp.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["tamucodezeta@gmail.com"],
+  password: ENV["CodeZetaPassword"]
   }
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
