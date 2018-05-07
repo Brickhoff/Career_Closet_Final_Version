830eb2e4980517687179427d3f063cc0e2f59c7c
EC2 Default User <ec2-user@ip-172-31-39-58.us-east-2.compute.internal>
2018-05-07 22:16:24 +0000

    test4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+  config.assets.digest = true
   
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = {:host => 'vast-savannah-25663.herokuapp.com'}
