909195e5fa3eb5ed17b241496b717694ab973da4
EC2 Default User <ec2-user@ip-172-31-39-58.us-east-2.compute.internal>
2018-05-07 23:31:19 +0000

    test5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   config.assets.digest = true
   
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = {:host => 'vast-savannah-25663.herokuapp.com'}
+  config.action_mailer.default_url_options = {:host => 'careercloset8.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
